c450ecdde4f080971cb2c94575b3d5d4cf7490e7
Yuyang Deng <dyyjason1212@163.com>
2025-07-29 21:55:01 +0800

    Add- All cosinor functions integrated (Ver 1.0)

--- CosinorRegressionModel(TRPTSD)/bayesian_model.py
+++ CosinorRegressionModel(TRPTSD)/bayesian_model.py
@@ -36,8 +36,74 @@ import os
 import copy
 from CosinorPy.helpers import df_add_row
 
+def load_data(path):
+    """Function used to parser the data using the file_parser method from the CosinorPy package"""
+    df = pd.read_csv(path)
+    df['Region start time'] = pd.to_datetime(df['Region start time'])
+    df['date'] = df['Region start time'].dt.date
+    df['hour'] = df['Region start time'].dt.hour
+    df = df.drop('Unnamed: 0', axis=1)
+    return df
+
+pre_data = load_data("data/RNS_G_Pre_output.csv")
+post_data = load_data("data/RNS_G_M1_output.csv")
+
+def prepare_data_pre(df):
+    """Function used to prepare the data for Cosinor Regression"""
+    df1 = df.copy()
+    
+    # Ensure date column is in datetime format
+    df1['date'] = pd.to_datetime(df1['date'])
+
+    # Compute 'week' column by calculating the number of days since the start date
+    start_date = df1['date'].min()
+    df1['week'] = ((df1['date'] - start_date).dt.days // 7) + 1
+    df1['week'] = df1['week'].apply(lambda x: f"week{x}")
+
+    # Add remaining columns
+    df1['test'] = df1['week'].astype(str)
+    df1['x'] = df1['hour']
+    df1['y'] = df1["Pattern A Channel 2"]
+
+    return df1
+
+def prepare_data_post(df):
+    """Function used to prepare the data for Cosinor Regression"""
+    df1 = df.copy()
+    
+    # Ensure date column is in datetime format
+    df1['date'] = pd.to_datetime(df1['date'])
+
+    # Compute 'week' column by calculating the number of days since the start date
+    start_date = df1['date'].min()
+    df1['week'] = ((df1['date'] - start_date).dt.days // 7) + 6
+    df1['week'] = df1['week'].apply(lambda x: f"week{x}")
+
+    # Add remaining columns
+    df1['test'] = df1['week'].astype(str)
+    df1['x'] = df1['hour']
+    df1['y'] = df1["Pattern A Channel 2"]
+
+    return df1
+
+df_results_pre_data = cosinor.population_fit_group(prepare_data_pre(pre_data), n_components = [1,2,3], period=24, plot=False)
+df_best_models_pre_data = cosinor.get_best_models_population(prepare_data_pre(pre_data), df_results_pre_data, n_components = [1,2,3])
+cosinor.plot_df_models_population(prepare_data_pre(pre_data), df_best_models_pre_data)
+df_best_models_pre_data
+
+df_results_post_data = cosinor.population_fit_group(prepare_data_post(post_data), n_components = [1,2,3], period=24, plot=False)
+df_best_models_post_data = cosinor.get_best_models_population(prepare_data_post(pre_data), df_results_post_data, n_components = [1,2,3])
+cosinor.plot_df_models_population(prepare_data_post(post_data), df_best_models_post_data)
+df_best_models_post_data
+
+df_best_models_pre_data
+df_best_models_post_data
+test_statistics = pd.concat([df_best_models_pre_data, df_best_models_post_data], axis=0, ignore_index=True)
+test_statistics
+
+
 # Load the cosinor parameters DataFrame
-params_df = pd.read_csv('cosinor_parameters.csv')
+params_df = test_statistics[['mean(amplitude)', 'mean(acrophase)', 'mean(mesor)']].copy()
 
 # Extract the relevant columns
 amplitude = params_df['mean(amplitude)'].values
