4de4a18d045101879dc5fcbfa52ec17fff8b7748
DYYasUNCStudent <dyy12@unc.edu>
2025-08-29 19:45:48 -0400

    Add- modules imports for logistic regression

--- LogisticRegreesionModel(TRPTSD)/logistic_regression_model.py
+++ LogisticRegreesionModel(TRPTSD)/logistic_regression_model.py
@@ -1,3 +1,13 @@
 """
-
+This module contains the implementation of a logistic regression model for predicting LFP data from TRPTSD data.
+This module is part of the TRPTSD project for the Suthana Lab Group.
 """
+
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+
+
+# Data Loading and Preprocessing
