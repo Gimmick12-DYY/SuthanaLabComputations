55fe1372d37312b7060ad55aaf73478ca2bc39a6
DYYasUNCStudent <dyy12@unc.edu>
2025-08-29 21:19:27 -0400

    Add- data loading section

--- LogisticRegreesionModel(TRPTSD)/logistic_regression_model.py
+++ LogisticRegreesionModel(TRPTSD)/logistic_regression_model.py
@@ -11,3 +11,17 @@ from sklearn.model_selection import train_test_split
 
 
 # Data Loading and Preprocessing
+def load_data(trptsd_path, lfp_path):
+    """
+    Load TRPTSD and LFP data from CSV files.
+    
+    Parameters:
+    trptsd_path (str): Path to the TRPTSD data CSV file.
+    lfp_path (str): Path to the LFP data CSV file.
+    
+    Returns:
+    pd.DataFrame, pd.DataFrame: Loaded TRPTSD and LFP data.
+    """
+    trptsd_data = pd.read_csv(trptsd_path)
+    lfp_data = pd.read_csv(lfp_path)
+    return trptsd_data, lfp_data
